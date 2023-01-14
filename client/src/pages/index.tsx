ac41238e8fb383c26bb8bdfcdad108c796e06eff
Mehedi Hasan <mehedi08h@gmail.com>
2023-01-14 22:29:39 +0600

    setup client

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { Box, Text } from "@chakra-ui/react";
+export default function Home() {
+    return (
+        <Box>
+            <Text color="darkblue">Hello from chat app</Text>
+        </Box>
+    );
+}
