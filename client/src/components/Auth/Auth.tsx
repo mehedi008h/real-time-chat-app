babd4321e479f5dfe1d8c32cba1357904872b629
Mehedi Hasan <mehedi08h@gmail.com>
2023-01-15 11:38:41 +0600

    form validation with formik & yup

--- client/src/components/Auth/Auth.tsx
+++ client/src/components/Auth/Auth.tsx
@@ -15,8 +15,25 @@ import { FaFacebook } from "react-icons/fa";
 import { RiChatSmile2Line } from "react-icons/ri";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import React, { FC, useState } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 interface AuthProps {}
+
+interface Values {
+    email: string;
+    password: string;
+}
+
+// Creating schema
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .required("Email is a required field")
+        .email("Invalid email format"),
+    password: Yup.string()
+        .required("Password is a required field")
+        .min(8, "Password must be at least 8 characters"),
+});
 
 const Auth: FC<AuthProps> = (props) => {
     // state
@@ -73,65 +90,129 @@ const Auth: FC<AuthProps> = (props) => {
                     >
                         or with email
                     </Text>
-                    <Stack width="300px" spacing={4}>
-                        <Input
-                            type="email"
-                            border="1px"
-                            borderColor="blackAlpha.500"
-                            focusBorderColor="blackAlpha.500"
-                            placeholder="Enter your email.."
-                            _placeholder={{ color: "blackAlpha.500" }}
-                            _hover={{ border: "1px" }}
-                            _focus={{ border: "1px" }}
-                        />
-                        {signup && (
-                            <Input
-                                type="text"
-                                border="1px"
-                                borderColor="blackAlpha.500"
-                                focusBorderColor="blackAlpha.500"
-                                placeholder="Enter your name.."
-                                _placeholder={{ color: "blackAlpha.500" }}
-                                _hover={{ border: "1px" }}
-                                _focus={{ border: "1px" }}
-                            />
-                        )}
 
-                        <InputGroup size="md">
-                            <Input
-                                pr="4.5rem"
-                                type={show ? "text" : "password"}
-                                border="1px"
-                                borderColor="blackAlpha.500"
-                                focusBorderColor="blackAlpha.500"
-                                placeholder="Enter your password.."
-                                _placeholder={{ color: "blackAlpha.500" }}
-                                _hover={{ border: "1px" }}
-                                _focus={{ border: "1px" }}
-                            />
-                            <InputRightElement width="4.5rem">
-                                <Button
-                                    h="1.75rem"
-                                    size="sm"
-                                    onClick={handleClick}
-                                    color="blackAlpha.600"
-                                >
-                                    {show ? (
-                                        <AiOutlineEyeInvisible size={20} />
-                                    ) : (
-                                        <AiOutlineEye size={20} />
-                                    )}
-                                </Button>
-                            </InputRightElement>
-                        </InputGroup>
-                        <Button
-                            bg="blue.600"
-                            color="white"
-                            _hover={{ bg: "blue.500" }}
-                        >
-                            {signup ? "Signup" : "Login"}
-                        </Button>
-                    </Stack>
+                    <Formik
+                        validationSchema={schema}
+                        initialValues={{
+                            email: "",
+                            password: "",
+                        }}
+                        onSubmit={(values: Values) => {
+                            console.log(values);
+                        }}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                        }) => (
+                            <form noValidate onSubmit={handleSubmit}>
+                                <Stack width="300px" spacing={4}>
+                                    <Box>
+                                        <Input
+                                            type="email"
+                                            border="1px"
+                                            borderColor={
+                                                errors.email && touched.email
+                                                    ? "red"
+                                                    : "blackAlpha.500"
+                                            }
+                                            focusBorderColor="blackAlpha.500"
+                                            placeholder="Enter your email.."
+                                            _placeholder={{
+                                                color: "blackAlpha.500",
+                                            }}
+                                            _hover={{ border: "1px" }}
+                                            _focus={{ border: "1px" }}
+                                            name="email"
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.email}
+                                        />
+                                        {/* If validation is not passed show errors */}
+                                        <Text
+                                            color="red.400"
+                                            fontSize={14}
+                                            marginTop={2}
+                                        >
+                                            {errors.email &&
+                                                touched.email &&
+                                                errors.email}
+                                        </Text>
+                                    </Box>
+
+                                    <Box>
+                                        <InputGroup size="md">
+                                            <Input
+                                                pr="4.5rem"
+                                                type={
+                                                    show ? "text" : "password"
+                                                }
+                                                border="1px"
+                                                borderColor={
+                                                    errors.password &&
+                                                    touched.password
+                                                        ? "red"
+                                                        : "blackAlpha.500"
+                                                }
+                                                focusBorderColor="blackAlpha.500"
+                                                placeholder="Enter your password.."
+                                                _placeholder={{
+                                                    color: "blackAlpha.500",
+                                                }}
+                                                _hover={{ border: "1px" }}
+                                                _focus={{ border: "1px" }}
+                                                name="password"
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.password}
+                                            />
+                                            <InputRightElement width="4.5rem">
+                                                <Button
+                                                    h="1.75rem"
+                                                    size="sm"
+                                                    onClick={handleClick}
+                                                    color="blackAlpha.600"
+                                                >
+                                                    {show ? (
+                                                        <AiOutlineEyeInvisible
+                                                            size={20}
+                                                        />
+                                                    ) : (
+                                                        <AiOutlineEye
+                                                            size={20}
+                                                        />
+                                                    )}
+                                                </Button>
+                                            </InputRightElement>
+                                        </InputGroup>
+                                        {/* If validation is not passed show errors */}
+                                        <Text
+                                            color="red.400"
+                                            fontSize={14}
+                                            marginTop={2}
+                                        >
+                                            {errors.password &&
+                                                touched.password &&
+                                                errors.password}
+                                        </Text>
+                                    </Box>
+                                    <Button
+                                        type="submit"
+                                        bg="blue.600"
+                                        color="white"
+                                        _hover={{ bg: "blue.500" }}
+                                    >
+                                        {signup ? "Signup" : "Login"}
+                                    </Button>
+                                </Stack>
+                            </form>
+                        )}
+                    </Formik>
+
                     <Button
                         color="blackAlpha.700"
                         fontSize={13}
